447c8b3075f383546c9b4472aab05895e8109b41
ivaan1992 <ivaan.lg.92@gmail.com>
2021-11-15 20:37:30 -0600

    Start mobile version

---
+++ script.js
@@ -0,0 +1,16 @@
+// Mobile menu
+/*const menu = document.querySelector('.cont-menu');
+const hamburger = document.querySelector('.mobile-menu-button');
+const xIcon = document.querySelector('.xIcon');
+
+function toggleMenu() {
+    if (menu.classList.contains('showMenu')) {
+        menu.classList.remove('showMenu');
+        xIcon.style.display = 'none';
+    } else {
+        menu.classList.add('showMenu');
+        xIcon.style.display = 'block'
+    }
+}
+
+hamburger.addEventListener('click', toggleMenu);*/
